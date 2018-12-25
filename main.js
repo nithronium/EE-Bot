c99309f45a578240532a343ec678bd52c0bd53c9
nithronium <45077058+nithronium@users.noreply.github.com>
2018-12-25 16:19:23 +0300

    Added new function "!colorme"
    !colorme function allows users to get a color from pre-defined color set on the server. This would assign a role to the user with a name of the color and role color will be the color asked by user. If role doesn't exist, bot will create the role and then assign the user.

--- main.js
+++ main.js
@@ -30,6 +30,44 @@ client.on('message', msg => {
 }
  );
 
+//New Command !colorme colorname, allows users to get their own color on server
+//Define colors below before the code piece in the form of;
+// "color-name" : "hex-code-of-color"
+var colors = {
+	"salmon" : "FA8072",
+	"red" : "FF0000",
+	"darkred" : "8B0000",
+	"golden" : "FFD700"}
+
+client.on('message', msg => {
+	var colorrole = msg.content;
+	colorrole = colorrole.split(" ");
+	if (colorrole[0] === "!colorme")
+	{
+		var colorexist = Object.keys(colors).includes(colorrole[1]);
+		let colorhex = wtf8.decode(wtf8.encode(colorrole[1]));
+		colorhex = colors[colorhex];
+
+		if (colorexist) {
+			var role = msg.guild.roles.find(r => r.name === colorrole[1]);
+			if (!role) {
+				msg.guild.createRole({
+					name: colorrole[1],
+					color: colorhex,
+							})
+					}
+			else {
+				console.log("role already exists"); 
+				}
+		let colorrolename = msg.guild.roles.find(role => role.name === colorrole[1]);
+		msg.member.setRoles([colorrolename]).catch(console.error);
+			
+					}
+		else {
+			msg.reply(wtf8.decode(wtf8.encode("I am sorry, I don't know what color is this :(")));
+			}
+	}
+});
 
  
 client.login('INSERT_YOUR_TOKEN_HERE');
